d262a0e1760811ebfc42c93ae5e65c6f6c90e542
laurentgarnier <l.garnier@msc-sgcc.com>
2015-10-10 14:38:42 +0200

    Commentaires et mise au "propre"

--- AlarmeMaison/Scripts python/Alarme.py
+++ AlarmeMaison/Scripts python/Alarme.py
@@ -13,22 +13,21 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email.MIMEImage import MIMEImage
 from email import encoders
- 
+
 # Horodatage de l'alarme
 dateDeLAlarme = datetime.datetime.now()  # on recupere la date actuelle
-timestampDeLAlarme = time.mktime(dateDeLAlarme.timetuple()) 
+timestampDeLAlarme = time.mktime(dateDeLAlarme.timetuple())
 
 adresseCamera = "192.168.1.81"
 
 # Creation du repertoire de stockage des images de l'alarme
 repertoire = os.path.join("/mnt/sda1/arduino/Captures", str(timestampDeLAlarme))
-os.mkdir(repertoire, 0777) 
+os.mkdir(repertoire, 0777)
 
 # Lancement des captures d'image depuis la camera
-# Lancement des acquisitions 
-
 httpServ = httplib.HTTPConnection(adresseCamera, "81")
 httpServ.connect()
+# Lancement des acquisitions
 for indexImage in range(10) :
     httpServ.request('GET', "/snapshot.cgi?user=admin&pwd=caslo7774")
     # attente de la reponse
@@ -43,15 +42,19 @@ for indexImage in range(10) :
 httpServ.close()
 
 # envoi du mail
-fichiers = os.listdir(repertoire)
 
+# Liste les images du répertoire
+fichiers = os.listdir(repertoire)
+# creation des donnes du mail
 msg = MIMEMultipart()
 msg['From'] = "lologar69@gmail.com"
 msg['To'] = "l.garnier@msc-sgcc.com"
-msg['Subject'] = "Ouverture de la porte de la veranda" 
-message = "La veranda a ete ouverte"
-msg.attach(MIMEText(message))
+msg['Subject'] = "Ouverture de la porte de la veranda"
 
+objetDuMail = "La veranda a ete ouverte"
+msg.attach(MIMEText(objetDuMail))
+
+# on ajoute les fichiers en pieces jointes
 for f in fichiers or []:
         with open(os.path.join(repertoire, f), "rb") as fil:
             msg.attach(MIMEImage(
@@ -59,10 +62,13 @@ for f in fichiers or []:
                 Content_Disposition='attachment; filename="%s"' % basename(f)
             ))
 
+# log sur le serveur de messagerie
 mailserver = smtplib.SMTP('smtp.gmail.com', 587)
 mailserver.starttls()
 mailserver.login('lologar69@gmail.com', 'laur*nt2')
-listeDestinataires = ["l.garnier@msc-sgcc.com","estelle.crozier@wanadoo.fr", "lologar69@gmail.com"]
+
+listeDestinataires = ["l.garnier@msc-sgcc.com","estelle.crozier@wanadoo.fr"]
+# envoi a tous les destinataires
 for destinataire in listeDestinataires :
 	mailserver.sendmail('lologar69@gmail.com', destinataire, msg.as_string())
 mailserver.quit()
--- rf433/recepteur.ino/recepteur.ino.ino
+++ rf433/recepteur.ino/recepteur.ino.ino
@@ -5,17 +5,19 @@ uint8_t buflen = VW_MAX_MESSAGE_LEN; // Taille maximum du buffer
 
 bool enAttente;
 #define LED 11
+#define PIN_RECEPTION 2
 
 void setup()
 {
     Serial.begin(9600); // Initialisation du port série pour avoir un retour sur le serial monitor
     vw_setup(2400);	// Initialisation de la librairie VirtualWire à 2400 bauds  (maximum de mon module)
-    vw_set_rx_pin(2);   // On indique que l'on veut utiliser la broche 2 pour reçevoir
+    vw_set_rx_pin(PIN_RECEPTION);
     vw_rx_start();      // Activation de la partie réception de la librairie VirtualWire
     pinMode(LED, OUTPUT);
     digitalWrite(LED, LOW);
     enAttente = true;
 }
+
 void loop()
 {
     if(enAttente)
@@ -23,25 +25,31 @@ void loop()
         Serial.println("En attente");
         enAttente = false;
     }
-          
-    if (vw_have_message()) // Si on a un message dans le buffer
+
+    // Si on a un message dans le buffer
+    if (vw_have_message())
     {
-        if (vw_get_message(buf, &buflen)) // Alors on récupère ce message qu'il soit entier ou pas
+        // On récupère ce qu'on a complet ou pas
+        if (vw_get_message(buf, &buflen))
         {
             int i;
             String msg;
             Serial.print("RX : ");
-            for (i = 0; i < buflen; i++) // On affiche tous ce que l'on a
+            // On affiche le message
+            for (i = 0; i < buflen; i++)
             {
               Serial.print(buf[i]);
               msg += buf[i];
             }
-            
-            Serial.println(""); // Une fois qu'il est entier, on peut ajouter un retour à la ligne.
+
+            // Une fois qu'il est entier, on peut ajouter un retour à la ligne.
+            Serial.println("");
+
+            // on traite le message
             int etat;
             if( msg.toInt() > 1)
               etat = LOW;
-            else 
+            else
               etat = HIGH;
             digitalWrite(LED, etat);
             enAttente = true;
