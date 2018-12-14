dc19d368501153db26eecfb26423909344e9e6b4
laurentgarnier <laurentgarnier@users.noreply.github.com>
2018-12-14 21:45:37 +0100

    Update CentraleAlarme.ino
    Correction pour envoyer un mail qu'en cas d'ouverture
    Préparation à la gestion de la porte du garage et de la porte du jardin

--- AlarmeMaison/Sketchs/YUN/CentraleAlarme/CentraleAlarme.ino
+++ AlarmeMaison/Sketchs/YUN/CentraleAlarme/CentraleAlarme.ino
@@ -1,12 +1,28 @@
 #include <Process.h>
 
 #define pinCapteurVeranda 2
-#define ledDebug 13
-#define OUVERT 1
-#define FERME 0
+#define pinCapteurPorteGarage 3
+#define pinCapteurPorteJardin 4
 
-int etat = FERME;
-int etatPrecedent = FERME;
+#define ledDebug 13
+
+#define VERANDA_OUVERTE 1
+#define VERANDA_FERMEE 0
+
+#define GARAGE_OUVERT 1
+#define GARAGE_FERME 0
+
+#define JARDIN_OUVERT 1
+#define JARDIN_FERME 0
+
+int etatCourantVeranda = VERANDA_FERMEE;
+int etatPrecedentVeranda = VERANDA_FERMEE;
+
+int etatCourantGarage = GARAGE_FERME;
+int etatPrecedentGarage = GARAGE_FERME;
+
+int etatCourantJardin = JARDIN_FERME;
+int etatPrecedentJardin = JARDIN_FERME;
 
 // initialisation du soft
 void setup()
@@ -18,30 +34,34 @@ void setup()
   
   // Initialisation de l'entree du capteur d'ouverture en pull up
   pinMode(pinCapteurVeranda, INPUT_PULLUP);
+  pinMode(pinCapteurPorteGarage, INPUT_PULLUP);
+  pinMode(pinCapteurPorteJardin, INPUT_PULLUP);
   
    // utilisation de la led de debug
-  digitalWrite(ledDebug, etat);
+  digitalWrite(ledDebug, etatCourantVeranda);
 }
 
 // boucle principale
 void loop()
 {
-  // Le changement d'etat de la porte est une opposition entre etat et etatPrecedent
-  // etat = OUVERT et etatPrecedent = FERME alors on vient d'ouvrir la porte
-  // etatPrecedent = OUVERT et  etat = FERME alors on vient de fermer la porte
+  // Le changement d'etat de la porte est une opposition entre etatCourantVeranda et etatPrecedentVeranda
+  // etatCourantVeranda = VERANDA_OUVERTE et etatPrecedentVeranda = VERANDA_FERMEE alors on vient d'ouvrir la porte
+  // etatPrecedentVeranda = VERANDA_OUVERTE et  etatCourantVeranda = VERANDA_FERMEE alors on vient de fermer la porte
   
-  etat = digitalRead(pinCapteurVeranda);
+  etatCourantVeranda = digitalRead(pinCapteurVeranda);
+  etatCourantJardin = digitalRead(pinCapteurPorteGarage);
+  etatCourantGarage = digitalRead(pinCapteurPorteJardin);
   
   // ouverture de la porte, on envoie le mail
-  if(etat == OUVERT && etatPrecedent == FERME)
+  if(etatCourantVeranda == VERANDA_OUVERTE && etatPrecedentVeranda == VERANDA_FERMEE)
   {
       GererOuvertureVeranda();
       return;
   }
 
-  // fermeture de la porte
-  if(etat == FERME && etatPrecedent == OUVERT)
-    etatPrecedent = FERME;
+//  // fermeture de la porte
+//  if(etatCourantVeranda == VERANDA_FERMEE && etatPrecedentVeranda == VERANDA_OUVERTE)
+//    etatPrecedentVeranda = VERANDA_FERMEE;
 
   digitalWrite(ledDebug, LOW);
 }
@@ -50,7 +70,7 @@ void loop()
 void GererOuvertureVeranda()
 {
   // utilisation de la led de debug pour afficher l etat du capteur
-  digitalWrite(ledDebug, etat);
+  digitalWrite(ledDebug, etatCourantVeranda);
   
   Process p;
   // Lancement du script python
@@ -58,7 +78,7 @@ void GererOuvertureVeranda()
   p.addParameter("/mnt/sda1/arduino/Alarme.py");
   p.run();
   // je considère la porte ouverte lorsque le mail a été envoyé
-  etatPrecedent = OUVERT;
+  etatPrecedentVeranda = digitalRead(pinCapteurVeranda);
   
   FaireClignoterLaLEDDeDebug();
 }
@@ -74,5 +94,3 @@ void FaireClignoterLaLEDDeDebug()
     delay(500);
   }
 }
-
-
